d53b496e0474f6a17442406557fa5f342810ab5d
Priyanzxx <kugapriyansusikumar2004@gmail.com>
2025-05-29 11:01:50 +0000

    first commit

---
+++ my/src/App.js
@@ -0,0 +1,14 @@
+
+import './assects/style.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+   <div>
+    <Header></Header>
+    <h1>welcome</h1>
+   </div>
+  );
+}
+
+export default App;
---
+++ my/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Header() {
+    return (
+     <div>
+    
+      <h1>Helo world</h1>
+     </div>
+    );
+  }
+  
+  export default Header;
+  
